55a9777206a1996deb9123c3670dc7cb406d82b6
Alonso <alonsoastoray21@gmail.com>
2024-01-24 18:10:36 -0500

    Modpuestos

---
+++ SistemaInventarioActivos/frmPuestosTrabajo.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPuestosTrabajo
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblTotalPuestos = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.dgvPuestos = New System.Windows.Forms.DataGridView()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.txtDescripcionP = New System.Windows.Forms.TextBox()
+        Me.txtNombreP = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.IdPuesto = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NombreP = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DescripcionP = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel1.SuspendLayout()
+        CType(Me.dgvPuestos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lblTotalPuestos)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.txtBuscar)
+        Me.Panel1.Controls.Add(Me.dgvPuestos)
+        Me.Panel1.Location = New System.Drawing.Point(24, 168)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(525, 249)
+        Me.Panel1.TabIndex = 33
+        '
+        'lblTotalPuestos
+        '
+        Me.lblTotalPuestos.AutoSize = True
+        Me.lblTotalPuestos.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalPuestos.Location = New System.Drawing.Point(122, 227)
+        Me.lblTotalPuestos.Name = "lblTotalPuestos"
+        Me.lblTotalPuestos.Size = New System.Drawing.Size(43, 16)
+        Me.lblTotalPuestos.TabIndex = 4
+        Me.lblTotalPuestos.Text = "Total"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(62, 227)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(54, 13)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "Registros:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(15, 22)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 13)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Buscar"
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Location = New System.Drawing.Point(200, 15)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(312, 20)
+        Me.txtBuscar.TabIndex = 1
+        '
+        'dgvPuestos
+        '
+        Me.dgvPuestos.AllowUserToAddRows = False
+        Me.dgvPuestos.AllowUserToDeleteRows = False
+        Me.dgvPuestos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvPuestos.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdPuesto, Me.NombreP, Me.DescripcionP})
+        Me.dgvPuestos.Location = New System.Drawing.Point(18, 48)
+        Me.dgvPuestos.Name = "dgvPuestos"
+        Me.dgvPuestos.ReadOnly = True
+        Me.dgvPuestos.RowHeadersVisible = False
+        Me.dgvPuestos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvPuestos.Size = New System.Drawing.Size(505, 169)
+        Me.dgvPuestos.TabIndex = 0
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(393, 120)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(75, 42)
+        Me.btnBuscar.TabIndex = 32
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(474, 120)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 42)
+        Me.btnCancelar.TabIndex = 31
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(474, 72)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 42)
+        Me.btnEliminar.TabIndex = 30
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Location = New System.Drawing.Point(393, 72)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(75, 42)
+        Me.btnEditar.TabIndex = 29
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(474, 24)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 42)
+        Me.btnGuardar.TabIndex = 28
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(393, 24)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 42)
+        Me.btnNuevo.TabIndex = 27
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(12, 2)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(35, 20)
+        Me.txtId.TabIndex = 26
+        Me.txtId.Visible = False
+        '
+        'txtDescripcionP
+        '
+        Me.txtDescripcionP.Location = New System.Drawing.Point(89, 59)
+        Me.txtDescripcionP.Multiline = True
+        Me.txtDescripcionP.Name = "txtDescripcionP"
+        Me.txtDescripcionP.Size = New System.Drawing.Size(261, 78)
+        Me.txtDescripcionP.TabIndex = 25
+        '
+        'txtNombreP
+        '
+        Me.txtNombreP.Location = New System.Drawing.Point(89, 24)
+        Me.txtNombreP.Name = "txtNombreP"
+        Me.txtNombreP.Size = New System.Drawing.Size(261, 20)
+        Me.txtNombreP.TabIndex = 24
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 13)
+        Me.Label2.TabIndex = 23
+        Me.Label2.Text = "Descripcion"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(39, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 22
+        Me.Label1.Text = "Puesto"
+        '
+        'IdPuesto
+        '
+        Me.IdPuesto.DataPropertyName = "IdPuesto"
+        Me.IdPuesto.HeaderText = "ID"
+        Me.IdPuesto.Name = "IdPuesto"
+        Me.IdPuesto.ReadOnly = True
+        Me.IdPuesto.Visible = False
+        '
+        'NombreP
+        '
+        Me.NombreP.DataPropertyName = "NombreP"
+        Me.NombreP.HeaderText = "PUESTO DE TRABAJO"
+        Me.NombreP.Name = "NombreP"
+        Me.NombreP.ReadOnly = True
+        Me.NombreP.Width = 200
+        '
+        'DescripcionP
+        '
+        Me.DescripcionP.DataPropertyName = "DescripcionP"
+        Me.DescripcionP.HeaderText = "DESCRIPCION"
+        Me.DescripcionP.Name = "DescripcionP"
+        Me.DescripcionP.ReadOnly = True
+        Me.DescripcionP.Width = 302
+        '
+        'frmPuestosTrabajo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(558, 423)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.txtDescripcionP)
+        Me.Controls.Add(Me.txtNombreP)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmPuestosTrabajo"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Registro de Puestos de Trabajo"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.dgvPuestos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTotalPuestos As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtBuscar As TextBox
+    Friend WithEvents dgvPuestos As DataGridView
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents txtDescripcionP As TextBox
+    Friend WithEvents txtNombreP As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents IdPuesto As DataGridViewTextBoxColumn
+    Friend WithEvents NombreP As DataGridViewTextBoxColumn
+    Friend WithEvents DescripcionP As DataGridViewTextBoxColumn
+End Class
---
+++ SistemaInventarioActivos/frmPuestosTrabajo.vb
@@ -0,0 +1,168 @@
+﻿Imports System.Data.SqlClient
+
+Public Class frmPuestosTrabajo
+    Private Sub frmPuestosTrabajo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DesactivarControles()
+        tamañoformulario()
+        LlenarDatos()
+
+    End Sub
+
+    Sub DesactivarControles()
+        btnGuardar.Enabled = False
+        btnEditar.Enabled = False
+        btnEliminar.Enabled = False
+        btnCancelar.Enabled = False
+
+        txtNombreP.Enabled = False
+        txtDescripcionP.Enabled = False
+
+        btnNuevo.Enabled = True
+    End Sub
+    Sub ActivarControles()
+        btnGuardar.Enabled = True
+        btnEditar.Enabled = False
+        btnEliminar.Enabled = False
+        btnCancelar.Enabled = True
+
+        txtNombreP.Enabled = True
+        txtDescripcionP.Enabled = True
+
+        btnNuevo.Enabled = False
+    End Sub
+    Sub LimpiarControles()
+        txtNombreP.Text = ""
+        txtDescripcionP.Text = ""
+        txtId.Text = ""
+        txtBuscar.Text = ""
+    End Sub
+    Sub tamañoformulario()
+        Width = 605
+        Height = 205
+        Panel1.Visible = False
+    End Sub
+    Sub tamañoformularioBuscar()
+        Width = 605
+        Height = 465
+        Panel1.Visible = True
+        txtBuscar.Focus()
+        txtBuscar.BackColor = Color.Yellow
+    End Sub
+    Sub insertar()
+        Dim sql As String
+        Dim id As Integer
+        If MsgBox("Guardar Nuevo Registro", vbQuestion + vbYesNo, "Sistema de Inventario") = vbNo Then
+            Exit Sub
+
+        End If
+        adaptador = New SqlDataAdapter("SELECT * FROM Puestos Where NombreP = '" & txtNombreP.Text & "' ", obtenerConexion)
+        tabla.Clear()
+        adaptador.Fill(tabla)
+        If tabla.Rows.Count > 0 Then
+            txtNombreP.Text = tabla.Rows(0).Item("NombreP")
+            MsgBox("El registro ya existe en la base de datos ", vbInformation, "Sistema inventario")
+            Exit Sub
+
+        End If
+        If txtNombreP.Text = "" Or txtDescripcionP.Text = "" Then
+            MsgBox("Existen campos vacios ", vbInformation, "Sistema inventario")
+            Exit Sub
+        Else
+            sql = "INSERT INTO Puestos(NombreP, DescripcionP) VALUES ('" & txtNombreP.Text & "','" & txtDescripcionP.Text & "')"
+            Dim conect As New SqlConnection(obtenerConexion)
+            conect.Open()
+            Using comando As New SqlCommand(sql, conect)
+                id = comando.ExecuteScalar
+            End Using
+            conect.Close()
+            MsgBox("Registro realizado exitosamente ", vbInformation, "Sistema inventario")
+        End If
+    End Sub
+    Sub LlenarDatos()
+        Dim sql As String
+        sql = "SELECT * FROM Puestos"
+        Try
+            Dim tabla As New DataTable
+            adaptador = New SqlDataAdapter(sql, obtenerConexion)
+            adaptador.Fill(tabla)
+            dgvPuestos.DataSource = tabla
+            lblTotalPuestos.Text = tabla.Rows.Count
+
+        Catch ex As Exception
+            MsgBox("Se ha mostrado el siguiente error " + ex.ToString + "Sistema Inventario")
+        End Try
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        ActivarControles()
+
+
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        insertar()
+        LlenarDatos()
+        Call DesactivarControles()
+        LimpiarControles()
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        tamañoformularioBuscar()
+        btnCancelar.Enabled = True
+        btnBuscar.Enabled = False
+
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        tamañoformulario()
+        LimpiarControles()
+        DesactivarControles()
+        Panel1.Visible = False
+        btnCancelar.Enabled = False
+        btnBuscar.Enabled = True
+    End Sub
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+        'Editar()
+        DesactivarControles()
+        LlenarDatos()
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        'Eliminar()
+        DesactivarControles()
+        LlenarDatos()
+
+    End Sub
+
+    Private Sub txtBuscar_TextChanged(sender As Object, e As EventArgs) Handles txtBuscar.TextChanged
+        If txtBuscar.Text = "" Then
+            LlenarDatos()
+        End If
+        adaptador = New SqlDataAdapter("SELECT * FROM Puestos WHERE NombreP LIKE '%" & txtBuscar.Text & "%'", obtenerConexion)
+        tabla.Clear()
+        adaptador.Fill(tabla)
+        If tabla.Rows.Count > 0 Then
+            dgvPuestos.DataSource = tabla
+            lblTotalPuestos.Text = tabla.Rows.Count
+        Else
+            dgvPuestos.DataSource = ""
+        End If
+    End Sub
+
+    Private Sub dgvPuestos_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvPuestos.CellDoubleClick
+        On Error Resume Next
+        txtId.Text = CStr(dgvPuestos.Item("IdPuesto", dgvPuestos.CurrentCell.RowIndex).Value)
+        txtNombreP.Text = CStr(dgvPuestos.Item("NombreP", dgvPuestos.CurrentCell.RowIndex).Value)
+        txtDescripcionP.Text = CStr(dgvPuestos.Item("DescripcionP", dgvPuestos.CurrentCell.RowIndex).Value)
+
+        btnCancelar.Enabled = True
+        btnEditar.Enabled = True
+        btnEliminar.Enabled = True
+        btnNuevo.Enabled = False
+
+        txtNombreP.Enabled = True
+        txtDescripcionP.Enabled = True
+
+        txtNombreP.Focus()
+    End Sub
+End Class
